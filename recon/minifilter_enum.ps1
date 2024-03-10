d84814ca9bbbcd545ee0b527d6f44e53216e20e1
0xv1n <11021725+0xv1n@users.noreply.github.com>
2024-03-10 16:56:24 -0400

    Update minifilter_enum.ps1

--- recon/minifilter_enum.ps1
+++ recon/minifilter_enum.ps1
@@ -2,11 +2,9 @@
 # Minifilters are assigned a specific altitude by Microsoft. This will sit within a range that is specific to the function of the minifilter.
 #   e.g. Anti-Virus minifilters are assigned an altitude between 320,000 and 329,999.
 #   and encryption minifilters are assigned an altitude between 140,000 and 149,999.
-
 #  For file Writes, Altitudes are processed in descending order.
 #  For file Reads, Altitudes are processed in ascending order.
-
-So when writing anti-virus is handled before encryption, but when reading decryption is handled before anti-virus.
+# So when writing anti-virus is handled before encryption, but when reading decryption is handled before anti-virus.
 fltmc | Where-Object { $_ -match '^\s*([\w]+)\s+(\d+)\s+(\d+)\s+(\d+)' -and $matches[3] -ge 320000 -and $matches[3] -le 329999 } | ForEach-Object {
     $driverName = $matches[1]
     $altitude = [int]$matches[3]
@@ -19,8 +17,12 @@ fltmc | Where-Object { $_ -match '^\s*([\w]+)\s+(\d+)\s+(\d+)\s+(\d+)' -and $mat
             Altitude = $altitude
             Frame = [int]$matches[4]
             DisplayName = $driverInfo.DisplayName
-            StartType = $driverInfo.StartType
+            Start = $driverInfo.Start
+			StartType = $driverInfo.Type
             Status = $driverInfo.Status
+			ImagePath = $driverInfo.ImagePath
+			Group = "$driverInfo.Group"
         }
     }
 }
+
